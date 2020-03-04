a38f49323bdc6dab023699b1961e6f9c7d6182a8
cford14 <cford14@gmail.com>
2020-03-04 14:07:22 -0600

    commit build

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './App.css';
+import { HashRouter, Route, Link } from "react-router-dom";
 import { Banner } from './banner/Banner'
 import { Body } from './body/Body';
 
 function App() {
   return (
-    <div>
-      <head>
+    <HashRouter basename='/'>
+      <div>
+        <head>
           <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css"/>
           <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
           <script src="semantic/dist/semantic.min.js"></script>
-      </head>
-    <div className="App">
-      <Banner />
-      <Body />
-    </div>
-    </div>
+        </head>
+        <div className="App">
+          <Banner />
+          <Body />
+        </div>
+      </div>
+    </HashRouter>
   );
 }
 
