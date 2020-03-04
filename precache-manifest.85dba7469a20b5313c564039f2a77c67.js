e4b857898258f508959e9f29437812cc67bf9bda
cford14 <cford14@gmail.com>
2020-03-04 14:06:20 -0600

    Updates

---
+++ precache-manifest.85dba7469a20b5313c564039f2a77c67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c231d2ebc7c6643fb3c235d20664fbce",
+    "url": "/partialProject/index.html"
+  },
+  {
+    "revision": "ae1632d9134ae07903b7",
+    "url": "/partialProject/static/css/main.b76cc89a.chunk.css"
+  },
+  {
+    "revision": "ba8b665da174347b8645",
+    "url": "/partialProject/static/js/2.d0d8c1c8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/partialProject/static/js/2.d0d8c1c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1632d9134ae07903b7",
+    "url": "/partialProject/static/js/main.f96b0e9c.chunk.js"
+  },
+  {
+    "revision": "99125e95e0d480036f06",
+    "url": "/partialProject/static/js/runtime-main.f6c84947.js"
+  },
+  {
+    "revision": "b6c042b7f9e4dd78253c7eebb43be9a3",
+    "url": "/partialProject/static/media/storefront.b6c042b7.jpg"
+  }
+]);
